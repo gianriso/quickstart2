5755a0c1595c85cd3d53dcba25232d5b1a845415
19813779 <giancarlo.risopatron1601@alumnos.ubiobio.cl>
2021-09-07 20:28:25 -0300

    init proyect

--- front/App.js
+++ front/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  {SignInSide} from './login.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}> QuickService</Text>
       <StatusBar style="auto" />
+      <login>soy el index</login>
     </View>
   );
+}
+
+
+componentDidMount() {
+  SignInSide();
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +25,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  
 });
